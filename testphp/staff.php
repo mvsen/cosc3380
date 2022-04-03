285ff6c31cea14a6e295bd61648a03fcca77b7df
thunguuyen <102563429+thunguuyen@users.noreply.github.com>
2022-04-02 19:01:24 -0500

    Add files via upload

---
+++ testphp/staff.php
@@ -0,0 +1,48 @@
+<?php
+include_once 'header.php'
+
+?>
+
+<section class ="index-intro">
+        <?php
+                    if (isset($_SESSION["useruid"]))
+                    {
+                        echo "<p> Hello there " . $_SESSION['useruid'] . "</p>";
+                        
+                    }
+                    ?>
+        <h1> This is a Staff page </h1>
+        <section class ="add-employee-form">
+            <h2> Add an Employee</h2>
+            <div class="add-employee-form-form">
+            <form action="includes/signup.inc.php" method="post" >
+                <input type="text" name="name" placeholder="Full name...">
+                <input type="text" name="birthday" placeholder="DOB MM/DD/YYYY">
+                <input type="text" name="email" placeholder="email...">
+                <input type="text" name="uid" placeholder="Username...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
+                <p> Emergency Contact Information 1 </p>
+                <input type="text" name="emergencycontactname1" placeholder="Emergency Contact Name">
+                <input type="text" name="phone_number1" placeholder="Phone # (XXX-XXX-XXXX)">
+                <input type="text" name="relationship1" placeholder="Relationship">
+                <p> Emergency Contact Information 2 </p>
+                <input type="text" name="emergencycontactname2" placeholder="Emergency Contact Name">
+                <input type="text" name="phone_number2" placeholder="Phone # (XXX-XXX-XXXX)">
+                <input type="text" name="relationship2" placeholder="Relationship">
+                <button type="submit" name="submit"> Add Employee </button>
+
+            </form>
+            </div>
+            <?php
+
+        ?>
+   
+            <p> Here is some important information </p>
+        </section>
+
+
+        <?php
+include_once 'footer.php'
+
+?>
