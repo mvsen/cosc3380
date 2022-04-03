118b746a2c253e6c597340ae63a8afb55f5fc2e2
jdot08 <77464089+jdot08@users.noreply.github.com>
2022-04-03 14:57:57 -0500

    Update inventory.php

--- testphp/inventory.php
+++ testphp/inventory.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'header.php';
-
+require_once 'includes\dbh.inc.php';
 
 ?>
 
@@ -24,6 +24,31 @@ include_once 'header.php';
 
             <h1> This is a Inventory page</h1>
             <p> Here is some importnat information </p>
+            <table>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                </tr>
+            
+            <?php
+            $sql = "SELECT * FROM Products";
+            $result = $conn->query($sql);
+            // output data of each row
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>".$row["Pr_ID"]."</td>";
+                    echo "<td>".$row["Pr_Name"]."</td>";
+                    echo "<td>".$row["Pr_Quantity"]."</td>";
+                    echo "<td>".$row["Pr_Cost"]."</td>";
+                    echo "</tr>";
+                }
+              }
+            ?>
+            </table>
         </section>
 
 
