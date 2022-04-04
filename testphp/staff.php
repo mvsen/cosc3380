3b4fa853cffab08499100c78aa9d326a2bf95835
thunguuyen <102563429+thunguuyen@users.noreply.github.com>
2022-04-04 18:39:33 -0500

    Update staff.php

--- testphp/staff.php
+++ testphp/staff.php
@@ -15,33 +15,65 @@ include_once 'header.php'
         <section class ="add-employee-form">
             <h2> Add an Employee</h2>
             <div class="add-employee-form-form">
-
+            <form action="includes/staff.inc.php" method="post" >
                 <input type="text" name="name" placeholder="Full name...">
                 <input type="text" name="birthday" placeholder="DOB MM/DD/YYYY">
+                <input type="text" name="gender" placeholder="Gender">
                 <input type="text" name="phone_number" placeholder="Phone # (XXX-XXX-XXXX)">
-                
+                <input type="text" name="address" placeholder="Street, City, State, Zipcode">
                 <p> Create Account </p>
                 <input type="text" name="email" placeholder="email...">
                 <input type="text" name="uid" placeholder="Username...">
                 <input type="password" name="pwd" placeholder="Password...">
                 <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
-                <p> Emergency Contact Information 1 </p>
-                <input type="text" name="emergencycontactname1" placeholder="Emergency Contact Name">
-                <input type="text" name="ephone_number1" placeholder="Phone # (XXX-XXX-XXXX)">
-                <input type="text" name="relationship1" placeholder="Relationship">
-                <p> Emergency Contact Information 2 </p>
-                <input type="text" name="emergencycontactname2" placeholder="Emergency Contact Name">
-                <input type="text" name="ephone_number2" placeholder="Phone # (XXX-XXX-XXXX)">
-                <input type="text" name="relationship2" placeholder="Relationship">
                 <p> Job Description </p>                
                 <input type="text" name="wage" placeholder="Wage ($/hr)">
-                <input type="text" name="department" placeholder="Department">
                 <input type="text" name="job_title" placeholder="Job Title">
+                <input type="text" name="department" placeholder="Department">
+                <input type="text" name="worksAt" placeholder="Works at">
                 <button type="submit" name="submit"> Add Employee </button>
 
             </form>
             </div>
             <?php
+
+if (isset($_GET["error"]))
+{
+    if($_GET["error"] == "emptyinput")
+    {
+       echo "<p>Fill in all fields!</p>";
+    }
+    else if ($_GET["error"] == "invaliduid")
+    {
+       echo "<p>Choose a proper username!</p>";
+    }
+    else if ($_GET["error"] == "invalidemail")
+    {
+       echo "<p>Choose a proper email!</p>";
+    }
+    else if ($_GET["error"] == "passwordsdontmatch")
+    {
+       echo "<p>Make sure passwords match!!</p>";
+    }
+    else if ($_GET["error"] == "smtm1failed")
+    {
+       echo "<p>Something went wrong, try again.</p>";
+    }
+    else if ($_GET["error"] == "smtm2failed")
+    {
+       echo "<p>Something went wrong, try again.</p>";
+    }
+    else if ($_GET["error"] == "usernametaken")
+    {
+       echo "<p>Username already taken. Try something else.</p>";
+    }
+    else if ($_GET["error"] == "none")
+    {
+       echo "<p>You have succesfully signed the employee up!</p>";
+    }
+    
+}
+    
 
         ?>
    
