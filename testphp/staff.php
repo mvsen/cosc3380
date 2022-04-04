510bcea154531be06c578c8dcad36ea72117de80
thunguuyen <102563429+thunguuyen@users.noreply.github.com>
2022-04-04 17:15:48 -0500

    Update staff.php

--- testphp/staff.php
+++ testphp/staff.php
@@ -15,21 +15,28 @@ include_once 'header.php'
         <section class ="add-employee-form">
             <h2> Add an Employee</h2>
             <div class="add-employee-form-form">
-            <form action="includes/signup.inc.php" method="post" >
+
                 <input type="text" name="name" placeholder="Full name...">
                 <input type="text" name="birthday" placeholder="DOB MM/DD/YYYY">
+                <input type="text" name="phone_number" placeholder="Phone # (XXX-XXX-XXXX)">
+                
+                <p> Create Account </p>
                 <input type="text" name="email" placeholder="email...">
                 <input type="text" name="uid" placeholder="Username...">
                 <input type="password" name="pwd" placeholder="Password...">
                 <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
                 <p> Emergency Contact Information 1 </p>
                 <input type="text" name="emergencycontactname1" placeholder="Emergency Contact Name">
-                <input type="text" name="phone_number1" placeholder="Phone # (XXX-XXX-XXXX)">
+                <input type="text" name="ephone_number1" placeholder="Phone # (XXX-XXX-XXXX)">
                 <input type="text" name="relationship1" placeholder="Relationship">
                 <p> Emergency Contact Information 2 </p>
                 <input type="text" name="emergencycontactname2" placeholder="Emergency Contact Name">
-                <input type="text" name="phone_number2" placeholder="Phone # (XXX-XXX-XXXX)">
+                <input type="text" name="ephone_number2" placeholder="Phone # (XXX-XXX-XXXX)">
                 <input type="text" name="relationship2" placeholder="Relationship">
+                <p> Job Description </p>                
+                <input type="text" name="wage" placeholder="Wage ($/hr)">
+                <input type="text" name="department" placeholder="Department">
+                <input type="text" name="job_title" placeholder="Job Title">
                 <button type="submit" name="submit"> Add Employee </button>
 
             </form>
