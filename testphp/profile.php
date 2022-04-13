1a8a4d65a7e72762ef7f3824c1834083d9d6a88a
Your Name <you@example.com>
2022-04-13 17:57:56 -0500

    report

--- testphp/profile.php
+++ testphp/profile.php
@@ -41,12 +41,12 @@ include_once 'header.php'
                         //print_r($row);
                         $newid = $row['usersId'];
                     ?>
-                        <input type="text" name = id value="<?php echo $row['usersId']; ?>" placeholder="<?php echo $row['usersId']; ?>" readonly>
-                <input type="text" name="name" placeholder="Full name...">
-                <input type="text" name="email" placeholder="email...">
-                <input type="text" name="uid" placeholder="Username...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <input type="password" name="pwdrepeat" placeholder="Repeat Password...">
+                       User ID: <input type="text" name = id value="<?php echo $row['usersId']; ?>" placeholder="<?php echo $row['usersId']; ?>" readonly>
+                Full Name:<input type="text" name="name" placeholder="Full name...">
+                Email:<input type="text" name="email" placeholder="email...">
+                Username:<input type="text" name="uid" placeholder="Username...">
+                Password:<input type="password" name="pwd" placeholder="Password...">
+                Repeat Password:<input type="password" name="pwdrepeat" placeholder="Repeat Password...">
                 <button type="submit" name="update"> Update </button>
 
             </form>
@@ -64,6 +64,7 @@ include_once 'header.php'
 
 ?>
 </form></section>
+
 
 <section>
 
@@ -102,7 +103,7 @@ include_once 'header.php'
                 //print_r($row);
                 
                 
-                echo "<select name='department'>";
+                echo "Department: <select name='department'>";
                     while($row = mysqli_fetch_array($result)){
                         
                         echo "<option value='" . $row['D_Name'] ."'>" . $row['D_Name'] ."</option>";
@@ -119,9 +120,9 @@ include_once 'header.php'
                     //echo "<input type='date' name='day' placeholder='day'>;
                         
                     ?>
-                    <input type='text' name = 'id' value="<?php echo $newid; ?>" placeholder="<?php echo $newid; ?>" readonly>
-                    <input type='text' name='hours' placeholder='Hours Worked...'>
-                    <input type='date' name='day' placeholder='day'>  
+                    User ID :<input type='text' name = 'id' value="<?php echo $newid; ?>" placeholder="<?php echo $newid; ?>" readonly>
+                    Hours :<input type='text' name='hours' placeholder='Hours Worked...'>
+                    Date : <input type='date' name='day' placeholder='day'>  
                     <button type="submit" name="submit2"> Submit Hours</button>  
             </form>
             </div>
@@ -146,68 +147,73 @@ include_once 'header.php'
 <section>
 
 
+
+
+
+</section>
+<section>
 <p> Generate Work Report</p>
  
 
 
-  <div class="signup-form-form">
-      
-            <form action="includes/profile.inc.php" method="post" >
-                
-                <?php
-                    $serverName = "database-1.c8gxaoh2plvu.us-east-1.rds.amazonaws.com";
-                    $dBUsername = "admin";
-                    $dBPassword = "cosc3380";
-                    $dBname = "ZOOSchema";
-                    $conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dBname);
-                    $sql = "SELECT * FROM ZOOSchema.Departments;";
-                    $stmt = mysqli_stmt_init($conn);
-                if (!mysqli_stmt_prepare($stmt, $sql))
-                {
-                    header("location: ../profile.php?error=stmt1failed");
-                    exit();
-            
-                }
-                $username1 = $_SESSION['useruid'];
-               // mysqli_stmt_bind_param($stmt, "s", $username1);
-                mysqli_stmt_execute($stmt);
-            
-                $result = mysqli_stmt_get_result($stmt);
+ <div class="signup-form-form">
+     
+           <form method="get" >
+               
+               <?php
+                   $serverName = "database-1.c8gxaoh2plvu.us-east-1.rds.amazonaws.com";
+                   $dBUsername = "admin";
+                   $dBPassword = "cosc3380";
+                   $dBname = "ZOOSchema";
+                   $conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dBname);
+                   $sql = "SELECT * FROM ZOOSchema.Departments;";
+                   $stmt = mysqli_stmt_init($conn);
+               if (!mysqli_stmt_prepare($stmt, $sql))
+               {
+                   header("location: ../profile.php?error=stmt1failed");
+                   exit();
+           
+               }
+               $username1 = $_SESSION['useruid'];
+              // mysqli_stmt_bind_param($stmt, "s", $username1);
+               mysqli_stmt_execute($stmt);
+           
+               $result = mysqli_stmt_get_result($stmt);
 
-              
-            
-                //$row = mysqli_fetch_assoc($resultData);
-                //print_r($row);
-                $all = "All";
-                
-                echo "<select name='department'>";
-                    while($row = mysqli_fetch_array($result)){
-                        
-                        echo "<option value='" . $row['D_Name'] ."'>" . $row['D_Name'] ."</option>";
-                        
-                    }
-                    
-                    echo "<option value='" . $all ."' placeholder='All'</option>";
-                    echo "</select>";
-                    
-              
-                   // echo "<input type='text' name = 'id' value='". $newid." placeholder='". $newid." readonly>";
-                  //  echo "<input type='text' name='name' placeholder='Full name...'>";
-                    //echo "<input type='text' name='hours' placeholder='Hours Worked...'>";
-                    //echo "<input type='date' name='day' placeholder='day'>;
-                        
-                    ?>
-                    <input type='text' name = 'id' value="<?php echo $newid; ?>" placeholder="<?php echo $newid; ?>" readonly>
-                    <input type='date' name='day1' placeholder='Start Date...'>
-                    <input type='date' name='day2' placeholder='End Date'>  
-                    <button type="submit" name="submit2"> Generate Report</button>  
-            </form>
-            </div>
-            <?php
+             
+           
+               //$row = mysqli_fetch_assoc($resultData);
+               //print_r($row);
+               $all = "All";
+               
+               echo "Department: <select name='department'>";
+                   while($row = mysqli_fetch_array($result)){
+                       
+                       echo "<option value='" . $row['D_Name'] ."'>" . $row['D_Name'] ."</option>";
+                       
+                   }
+                   
+                   echo "<option value='" . $all ."' placeholder='All'</option>";
+                   echo "</select>";
+                   
+             
+                  // echo "<input type='text' name = 'id' value='". $newid." placeholder='". $newid." readonly>";
+                 //  echo "<input type='text' name='name' placeholder='Full name...'>";
+                   //echo "<input type='text' name='hours' placeholder='Hours Worked...'>";
+                   //echo "<input type='date' name='day' placeholder='day'>;
+                       
+                   ?>
+                   User ID: <input type='text' name = 'id' value="<?php echo $newid; ?>" placeholder="<?php echo $newid; ?>" readonly>
+                   Start Date: <input type='date' name='day1' placeholder='Start Date...'>
+                   End Date: <input type='date' name='day2' placeholder='End Date'>  
+                   <button type="submit" name="submit2"> Generate Report</button>  
+           </form>
+           </div>
+           <?php
 
-                    
-                    
-                    
+                   
+                   
+                   
 
 
 
@@ -216,6 +222,60 @@ include_once 'header.php'
 
 ?>
 
+<a = herf="#" onclick="widnow.print();"></a>
+<?php
+if (isset($_GET['department'])){
+$serverName = "database-1.c8gxaoh2plvu.us-east-1.rds.amazonaws.com";
+$dBUsername = "admin";
+$dBPassword = "cosc3380";
+$dBname = "ZOOSchema";
+$conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dBname);
+$dep = $_GET['department'];
+$day1 = $_GET['day1'];
+$day2 = $_GET['day2'];
+$username1 = $newid;
+$sql = "SELECT * FROM ZOOSchema.Hours where D_Name = ? and E_Id = ? and ZOOSchema.Hours.Date between ? and ?";
+$stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql))
+                {
+                    header("location: ../profile.php?error=stmt1failed");
+                    exit();
+            
+                }
+
+                mysqli_stmt_bind_param($stmt, "ssss", $dep, $username1, $day1, $day2);
+                mysqli_stmt_execute($stmt);
+            
+                $result = mysqli_stmt_get_result($stmt);
+                
+                $count = mysqli_num_rows($result);
+
+            echo "<br/>$count Records Found";
+            echo "<table border = '1' align = center'>";
+
+            echo "<tr>";
+            echo "<th>hours</th>";
+            echo "<th>Date</th>";
+            echo "<th>E_Id</th>";
+            echo "<th>D_Name</th>";
+            echo "<th>H_id</th>";
+            echo "</tr>";
+
+            while($row = mysqli_fetch_array($result))
+            {
+                echo "<tr>";
+                echo "<td>{$row['hours']}</td>";
+                echo "<td>{$row['Date']}</td>";
+                echo "<td>{$row['E_Id']}</td>";
+                echo "<td>{$row['D_Name']}</td>";
+                echo "<td>{$row['H_Id']}</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+            ?>
+
+        
 
 </section>
 
@@ -225,7 +285,7 @@ include_once 'header.php'
 
 
 
-<button type="submit" name="delete"> Delete My Account </button>
+
 <?php
 
 $serverName = "database-1.c8gxaoh2plvu.us-east-1.rds.amazonaws.com";
