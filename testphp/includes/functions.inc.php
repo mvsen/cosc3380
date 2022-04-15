53dd418687c44d6ff457cdb90ee665561007ef6e
gjgiron <98197378+gjgiron@users.noreply.github.com>
2022-04-14 20:54:16 -0500

    Add files via upload

--- testphp/includes/functions.inc.php
+++ testphp/includes/functions.inc.php
@@ -88,7 +88,7 @@ function uidExists($conn, $username, $email)
 
 function createUser($conn, $name, $email, $username, $pwd)
 {
-    $sql = "INSERT into ZOOSchema.users (usersName, usersEmail, usersUid, usersPwd) VALUES (?,?,?,?);";
+    $sql = "INSERT into ZOOSchema.users (usersName, usersEmail, usersUid, usersPwd, usersRank) VALUES (?,?,?,? ,?);";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql))
     {
@@ -98,7 +98,8 @@ function createUser($conn, $name, $email, $username, $pwd)
     }
 
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-    mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $username, $hashedPwd);
+    $uRank = "customer";
+    mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $username, $hashedPwd,  $uRank);
     mysqli_stmt_execute($stmt);
 
     mysqli_stmt_close($stmt);
@@ -110,7 +111,139 @@ function createUser($conn, $name, $email, $username, $pwd)
 
 
 
+
 }
+function updateHours($conn, $department, $id, $hours, $day)
+{
+    $sql = "INSERT into ZOOSchema.Hours (hours, Date, E_Id, D_Name) VALUES (?,?,?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../profilea.php?error=stmt2failed");
+        exit();
+
+    }
+
+
+    mysqli_stmt_bind_param($stmt, "isss", $hours, $day, $id, $department);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+
+
+    header("location: ../profilea.php?error=none21");
+    exit();
+
+
+
+
+}
+
+function modifyHours($conn, $department, $id, $hours, $day)
+{
+
+    $sql = "UPDATE ZOOSchema.users SET usersName =?, usersEmail = ?, usersUid = ?, usersPwd=? WHERE usersID = ?;";
+    $sql = "UPDATE ZOOSchema.Hours SET hours = ?, Date = ?, D_Name = ? WHERE E_Id = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../profile.php?error=stmt2failed");
+        exit();
+
+    }
+
+
+    mysqli_stmt_bind_param($stmt, "isss", $hours, $day, $id, $department);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+
+
+    header("location: ../profile.php?error=none21");
+    exit();
+
+
+
+
+}
+
+function deleteHours($conn, $department, $id, $day)
+{
+    $sql = "DELETE FROM ZOOSchema.Hours WHERE E_Id = ? and Date = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../profilea.php?error=stmt2failed");
+        exit();
+
+    }
+
+
+    mysqli_stmt_bind_param($stmt, "is", $id, $day);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+
+
+    header("location: ../profilea.php?error=none5");
+    exit();
+
+
+
+
+}
+
+function updateUser($conn, $name, $email, $username, $pwd, $id)
+{
+
+
+
+
+    $sql = "UPDATE ZOOSchema.users SET usersName =?, usersEmail = ?, usersUid = ?, usersPwd=? WHERE usersID = ?;";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../profile.php?error=stmt2failed");
+        exit();
+
+    }
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "ssssi", $name, $email, $username, $hashedPwd, $id);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    session_start();
+    session_unset();
+    session_destroy();
+
+    header("location: ../login.php");
+
+
+
+    //header("location: ../profile.php?error=okay");
+    exit();
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 function emptyInputLogin($username, $pwd) 
 {
     $result;
@@ -131,23 +264,113 @@ function loginUser($conn, $username, $pwd)
 
     if ($uidExists === false)
     {
-        header("location: ../login.php?error=wronglogin");
+        header("location: ../login.php?error=wronglogin1");
         exit();
     }
 
     $pwdHashed = $uidExists["usersPwd"];
     $checkPwd = password_verify($pwd, $pwdHashed);
     if ($checkPwd === false){
-        header("location: ../login.php?error=wronglogin");
+        header("location: ../login.php?error=wronglogin2");
     }
     else if ($checkPwd === true)
     {
         session_start();
         $_SESSION["userid"] = $uidExists["usersId"];
         $_SESSION["useruid"] = $uidExists["usersUid"];
+
+        
         header("location: ../index.php");
+
+        $sql = "SELECT usersRank from ZOOSchema.users where usersUid = ? OR usersEmail = ?;";
+        $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../signup.php?error=stmt1failed");
+        exit();
+
+    }
+    mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    $row = mysqli_fetch_assoc($resultData);
+    $_SESSION["userR"] = $row['usersRank'];
+
+    
+
+}
+}
+
+function createUserEmployee($conn, $name, $email, $username, $pwd)
+{
+    $sql = "INSERT into ZOOSchema.users (usersName, usersEmail, usersUid, usersPwd, usersRank) VALUES (?,?,?,?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../staff.php?error=stmt2failed");
         exit();
 
     }
 
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+    $rank = "employee";
+    mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $username, $hashedPwd, $rank);
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+
+
+    header("location: ../staff.php?error=none");
+    exit();
+
+}
+
+function createEmployee($conn, $name,$birthday,$gender, $email, $phone_number, $address, $wage, $job_title, $workHours, $department, $worksAt,$username,$pwd) {
+    $sql = "INSERT into ZOOSchema.Employees(E_Name, E_Birthdate, E_Gender, E_Email, E_Phonenumber, E_Address, E_Pay, E_JobTitle, E_WorkHours, E_Department, E_WorksAtS) VALUES (?,?,?,?,?,?,?,?,?,?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: ../staff.php?error=stmt2failed");
+        exit();
+
+    }
+
+    //$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "sssssssssss", $name,$birthday,$gender, $email, $phone_number, $address, $wage, $job_title,$workHours, $department, $worksAt );
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    createUserEmployee($conn, $name, $email, $username, $pwd);
+
+    header("location: ../staff.php?error=none");
+    exit();
+
+}
+
+
+function emptyInputEmployee($name,$birthday,$gender, $email, $phone_number, $address, $username, $pwd, $pwdrepeat, $wage, $job_title, $workHours, $department, $worksAt) 
+{
+    $result;
+
+    if (empty($name) || empty($birthday) || empty($gender) || empty($email) || empty($phone_number) || empty($address) || empty($username) || empty($pwd) || empty($pwdrepeat) || empty($wage) || empty($job_title) || empty($workHours) || empty($department) || empty($worksAt))
+    {
+        $result = true;
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+function invalidBDay($birthday) {
+    $result;
+    $max = new DateTime();
+    if($birthday>=$max) {
+        $result = false;
+    }
+    else{$result = true;}
+    return $result;
 }
